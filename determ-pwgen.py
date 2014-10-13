b5d28af1b8525b199f0aead249260b7fe98f78c2
I3ck <buckmartin@buckmartin.de>
2014-10-13 14:06:42 +0200

    Created class which contains all logic

--- determ-pwgen.py
+++ determ-pwgen.py
@@ -2,6 +2,23 @@ import hashlib, getpass, base64
 
 ROUNDS = 1000000
 
+class DetermPwgen:
+
+	def __init__(self, seed):
+		self._seed = seed
+
+	def generate_password(self, hostname, username, rounds):
+		pw = self._seed + hostname + username
+
+		for i in range(rounds):
+			pw = base64.b64encode((hashlib.sha256(pw).digest()))
+
+		return pw
+
+
+
+
+
 def main():
 	print "determ-pwgen created by I3ck https://github.com/I3ck/determ-pwgen"
 	print "licensed under the MIT License"
@@ -15,14 +32,13 @@ def main():
 			break
 		print "passwords don't match, please try again"
 
+	determPwgen = DetermPwgen(seed)
+
 	while True:
 		thing = raw_input('\n\nPlease enter the domain or name of the program (e.g. google or outlook): ')
 		username = raw_input('Please enter your username for ' + thing + ': ')
 
-		pw = seed + thing + username
-
-		for i in range(ROUNDS):
-			pw = base64.b64encode((hashlib.sha256(pw).digest()))
+		pw = determPwgen.generate_password(thing, username, ROUNDS)
 
 		print "\n" + username + " @ " + thing + ": " + pw
 
