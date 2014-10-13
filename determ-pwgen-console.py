ca0c9d407bfffa81f5b68b283aa27a56823f570d
I3ck <buckmartin@buckmartin.de>
2014-10-13 14:56:11 +0200

    It is now possible to define hostnames and usernames within a json file

--- determ-pwgen-console.py
+++ determ-pwgen-console.py
@@ -1,7 +1,31 @@
-import getpass
+import getpass, json
 from inc.DetermPwgen import *
 
 ROUNDS = 1000000
+
+
+def use_user_input(seed):
+	determPwgen = DetermPwgen(seed)
+
+	while True:
+		thing = raw_input('\n\nPlease enter the domain or name of the program (e.g. google or outlook): ')
+		username = raw_input('Please enter your username for ' + thing + ': ')
+
+		pw = determPwgen.generate_password(thing, username, ROUNDS)
+
+		print "\n" + username + " @ " + thing + ": " + pw
+
+
+def use_json_file(seed):
+	determPwgen = DetermPwgen(seed)
+
+	with open('accounts.json', 'r') as f:
+		accounts = json.load(f)
+
+	for account in accounts:
+		pw = determPwgen.generate_password(account['hostname'], account['username'], ROUNDS)
+		print "\n" + account['username'] + " @ " + account['hostname'] + ": " + pw
+
 
 def main():
 	print "determ-pwgen created by I3ck https://github.com/I3ck/determ-pwgen"
@@ -17,18 +41,20 @@ def main():
 				break
 			print "passwords don't match, please try again"
 
-		determPwgen = DetermPwgen(seed)
-
 		while True:
-			thing = raw_input('\n\nPlease enter the domain or name of the program (e.g. google or outlook): ')
-			username = raw_input('Please enter your username for ' + thing + ': ')
-
-			pw = determPwgen.generate_password(thing, username, ROUNDS)
-
-			print "\n" + username + " @ " + thing + ": " + pw
+			mode = raw_input('\n\nInput host- and usernames by hand[0]. Use accounts.json[1]: ')
+			if mode == "0":
+				use_user_input(seed)
+				break
+			elif mode == "1":
+				use_json_file(seed)
+				break
+			else:
+				print "unknown mode, please try again"
 
 	except KeyboardInterrupt:
 		print "program exiting"
+
 
 if __name__ == '__main__':
 	main()
