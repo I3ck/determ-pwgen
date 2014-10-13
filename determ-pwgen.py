e1f970074e6c607f9217165e13aa3f872a1e4ce8
I3ck <buckmartin@buckmartin.de>
2014-10-13 14:15:22 +0200

    now exiting properly on keyboard interrupt

--- determ-pwgen.py
+++ determ-pwgen.py
@@ -9,22 +9,27 @@ def main():
 	print "version2 (use version1 if you already generated passwords with it)"
 	print "using " + str(ROUNDS) + " rounds of sha256"
 
-	while True:
-		seed = getpass.getpass('password:')
-		seed2 = getpass.getpass('again:')
-		if seed == seed2:
-			break
-		print "passwords don't match, please try again"
 
-	determPwgen = DetermPwgen(seed)
+	try:
+		while True:
+			seed = getpass.getpass('password:')
+			seed2 = getpass.getpass('again:')
+			if seed == seed2:
+				break
+			print "passwords don't match, please try again"
 
-	while True:
-		thing = raw_input('\n\nPlease enter the domain or name of the program (e.g. google or outlook): ')
-		username = raw_input('Please enter your username for ' + thing + ': ')
+		determPwgen = DetermPwgen(seed)
 
-		pw = determPwgen.generate_password(thing, username, ROUNDS)
+		while True:
+			thing = raw_input('\n\nPlease enter the domain or name of the program (e.g. google or outlook): ')
+			username = raw_input('Please enter your username for ' + thing + ': ')
 
-		print "\n" + username + " @ " + thing + ": " + pw
+			pw = determPwgen.generate_password(thing, username, ROUNDS)
+
+			print "\n" + username + " @ " + thing + ": " + pw
+
+	except KeyboardInterrupt:
+		print "program exiting"
 
 if __name__ == '__main__':
 	main()
