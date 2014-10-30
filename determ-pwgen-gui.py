34603728f72644e3ca3d77982d9d8ad646447d90
I3ck <buckmartin@buckmartin.de>
2014-10-30 23:51:52 +0100

    passwords are now displayable

--- determ-pwgen-gui.py
+++ determ-pwgen-gui.py
@@ -1,5 +1,10 @@
 from Tkinter import *
+import tkMessageBox
 import json
+
+ROUNDS = 1000000 # todo should be defined at only one place
+
+from inc.DetermPwgen import *
 
 def main():
 	POSITIONS = {
@@ -75,7 +80,7 @@ def main():
 		usernameLabel = Label(root, text=account['username'])
 
 		calcButton = Button(	root, text="calc", width=SETTINGS['calc']['width'],
-								command=lambda hostname=account['hostname'], username=account['username'] : callback(hostname, username))
+								command=lambda hostname=account['hostname'], username=account['username'] : callback(hostname, username, pw1Entry.get(), pw2Entry.get()))
 
 		hostnameLabel.place(x=POSITIONS['hostname']['x'], y=y)
 		usernameLabel.place(x=POSITIONS['username']['x'], y=y)
@@ -97,9 +102,15 @@ def main():
 
 	root.mainloop()
 
-def callback(hostname, username):
-	# todo either fill a text field after username @ hostname or open a message box with the values
-    print "TODO : " + hostname + " @ " + username
+def callback(hostname, username, pw1, pw2): #todo rename pw1 and pw2 to seed1 and seed2
+	if pw1 != "" and pw1 == pw2:
+		determPwgen = DetermPwgen(pw1)
+		answer = determPwgen.generate_password(hostname, username, ROUNDS) # todo rename to pw once pw1 was renamed to seed
+		tkMessageBox.showinfo("Password for " + username + "@" + hostname , answer) # todo a dialog with copyable text
+
+	else:
+		tkMessageBox.showerror("Passwords don't match", "The passwords you provided don't match")
+
 
 
 if __name__ == '__main__':
