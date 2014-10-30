fa53c380f57733798d3db8c8e587ae70770e9570
I3ck <buckmartin@buckmartin.de>
2014-10-30 16:02:42 +0100

    Added labels for the password fields

--- determ-pwgen-gui.py
+++ determ-pwgen-gui.py
@@ -2,16 +2,30 @@ from Tkinter import *
 
 def main():
 	POSITIONS = {
+		'pw1Label' : {
+			'x' : 0,
+			'y' : 100
+		},
+		'pw2Label' : {
+			'x' : 0,
+			'y' : 120
+		},
 		'pw1Entry' : {
-			'x' : 10,
+			'x' : 100,
 			'y' : 100
 		},
 		'pw2Entry' : {
-			'x' : 10,
+			'x' : 100,
 			'y' : 120
 		}
 	}
 	SETTINGS = {
+		'pw1Label' : {
+			'text' : "Enter password: "
+		},
+		'pw2Label' : {
+			'text' : "Again: "
+		},
 		'pw1Entry' : {
 			'width' : 15,
 			'show' : "*"
@@ -24,15 +38,25 @@ def main():
 
 	root = Tk()
 	root.geometry("500x500")
+	root.title("determ-pwgen by I3ck")
+
+
+
+	pw1Label = Label(root, **SETTINGS['pw1Label'])
+	pw2Label = Label(root, **SETTINGS['pw2Label'])
 
 	pw1Entry = Entry(root, **SETTINGS['pw1Entry'])
 	pw2Entry = Entry(root, **SETTINGS['pw2Entry'])
 
 
+
+	pw1Label.place(**POSITIONS['pw1Label'])
+	pw2Label.place(**POSITIONS['pw2Label'])
+
 	pw1Entry.place(**POSITIONS['pw1Entry'])
 	pw2Entry.place(**POSITIONS['pw2Entry'])
 
-	root.title("determ-pwgen by I3ck")
+
 
 
 	root.mainloop()
