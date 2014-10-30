179f04fdfe267a57601e3f6dbe52f95ef4f7d94d
I3ck <buckmartin@buckmartin.de>
2014-10-30 15:57:54 +0100

    settings now saved in a dictionary

--- determ-pwgen-gui.py
+++ determ-pwgen-gui.py
@@ -11,12 +11,22 @@ def main():
 			'y' : 120
 		}
 	}
+	SETTINGS = {
+		'pw1Entry' : {
+			'width' : 15,
+			'show' : "*"
+		},
+		'pw2Entry' : {
+			'width' : 15,
+			'show' : "*"
+		}
+	}
 
 	root = Tk()
 	root.geometry("500x500")
 
-	pw1Entry = Entry(root, show="*", width=15)
-	pw2Entry = Entry(root, show="*", width=15)
+	pw1Entry = Entry(root, **SETTINGS['pw1Entry'])
+	pw2Entry = Entry(root, **SETTINGS['pw2Entry'])
 
 
 	pw1Entry.place(**POSITIONS['pw1Entry'])
