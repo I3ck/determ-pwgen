f95ea5b08a50a893c8e814e0851414bb2e45443a
I3ck <buckmartin@buckmartin.de>
2014-11-02 19:58:59 +0100

    resized password dialog and changed its title

--- determ-pwgen-gui.py
+++ determ-pwgen-gui.py
@@ -13,11 +13,10 @@ class PasswordDialog:
 
     def __init__(self, parent, hostname, username, pw):
 		top = self.top = Toplevel(parent)
-		top.geometry("200x75")
-		top.title("determ-pwgen by I3ck")
+		top.title(username + " @ " + hostname)
 		self.infoLabel = Label(top, text=username + " @ " + hostname)
 		self.infoLabel.pack()
-		self.pwEntry = Entry(top)
+		self.pwEntry = Entry(top, width=60)
 		self.pwEntry.pack()
 		self.pwEntry.insert(0, pw)
 
