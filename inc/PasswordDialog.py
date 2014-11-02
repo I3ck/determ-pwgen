62f2f9117258d6dfc403d04b954f20920461edd8
I3ck <buckmartin@buckmartin.de>
2014-11-02 20:03:49 +0100

    removed 'top' variable from PasswordDialog

--- inc/PasswordDialog.py
+++ inc/PasswordDialog.py
@@ -4,10 +4,10 @@ from Tkinter import *
 class PasswordDialog:
 
     def __init__(self, parent, hostname, username, pw):
-		top = self.top = Toplevel(parent)
-		top.title(username + " @ " + hostname)
-		self.infoLabel = Label(top, text=username + " @ " + hostname)
+		self.top = Toplevel(parent)
+		self.top.title(username + " @ " + hostname)
+		self.infoLabel = Label(self.top, text=username + " @ " + hostname)
 		self.infoLabel.pack()
-		self.pwEntry = Entry(top, width=60)
+		self.pwEntry = Entry(self.top, width=60)
 		self.pwEntry.pack()
 		self.pwEntry.insert(0, pw)
