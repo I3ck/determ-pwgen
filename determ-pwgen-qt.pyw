db3ed4f1044ebfae223cfbd7328374b94dc4af5e
Martin Buck <buckmartin@buckmartin.de>
2015-04-07 12:51:43 +0200

    added basic qt foundation

---
+++ determ-pwgen-qt.pyw
@@ -0,0 +1,17 @@
+import sys
+from PyQt4 import *
+
+from inc.form1 import *
+
+class MyForm(QtGui.QMainWindow):
+    def __init__(self, parent=None):
+        QtGui.QWidget.__init__(self, parent)
+        self.ui = Ui_mainWindow()
+        self.ui.setupUi(self)
+
+
+if __name__ == "__main__":
+    app = QtGui.QApplication(sys.argv)
+    myapp = MyForm()
+    myapp.show()
+    sys.exit(app.exec_())
---
+++ inc/form1.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'form1.ui'
+#
+# Created: Tue Apr  7 12:08:22 2015
+#      by: PyQt4 UI code generator 4.11.3
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt4 import QtCore, QtGui
+
+try:
+    _fromUtf8 = QtCore.QString.fromUtf8
+except AttributeError:
+    def _fromUtf8(s):
+        return s
+
+try:
+    _encoding = QtGui.QApplication.UnicodeUTF8
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig, _encoding)
+except AttributeError:
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig)
+
+class Ui_mainWindow(object):
+    def setupUi(self, mainWindow):
+        mainWindow.setObjectName(_fromUtf8("mainWindow"))
+        mainWindow.resize(800, 600)
+        mainWindow.setLocale(QtCore.QLocale(QtCore.QLocale.English, QtCore.QLocale.UnitedStates))
+        self.centralwidget = QtGui.QWidget(mainWindow)
+        self.centralwidget.setObjectName(_fromUtf8("centralwidget"))
+        self.widget = QtGui.QWidget(self.centralwidget)
+        self.widget.setGeometry(QtCore.QRect(50, 20, 203, 52))
+        self.widget.setObjectName(_fromUtf8("widget"))
+        self.gridLayout = QtGui.QGridLayout(self.widget)
+        self.gridLayout.setMargin(0)
+        self.gridLayout.setObjectName(_fromUtf8("gridLayout"))
+        self.label = QtGui.QLabel(self.widget)
+        self.label.setObjectName(_fromUtf8("label"))
+        self.gridLayout.addWidget(self.label, 0, 0, 1, 1)
+        self.lineEdit = QtGui.QLineEdit(self.widget)
+        self.lineEdit.setObjectName(_fromUtf8("lineEdit"))
+        self.gridLayout.addWidget(self.lineEdit, 0, 1, 1, 1)
+        self.label_2 = QtGui.QLabel(self.widget)
+        self.label_2.setObjectName(_fromUtf8("label_2"))
+        self.gridLayout.addWidget(self.label_2, 1, 0, 1, 1)
+        self.lineEdit_2 = QtGui.QLineEdit(self.widget)
+        self.lineEdit_2.setObjectName(_fromUtf8("lineEdit_2"))
+        self.gridLayout.addWidget(self.lineEdit_2, 1, 1, 1, 1)
+        mainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtGui.QMenuBar(mainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 19))
+        self.menubar.setObjectName(_fromUtf8("menubar"))
+        mainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtGui.QStatusBar(mainWindow)
+        self.statusbar.setObjectName(_fromUtf8("statusbar"))
+        mainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(mainWindow)
+        QtCore.QMetaObject.connectSlotsByName(mainWindow)
+
+    def retranslateUi(self, mainWindow):
+        mainWindow.setWindowTitle(_translate("mainWindow", "determ-pwgen by I3ck", None))
+        self.label.setText(_translate("mainWindow", "Enter Seed:", None))
+        self.label_2.setText(_translate("mainWindow", "Again:", None))
+
