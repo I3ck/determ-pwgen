8ddd8894ae803c40a08e8ac5d73c82e9d00608fa
Martin Buck <buckmartin@buckmartin.de>
2015-04-17 16:38:10 +0200

    DetermPwgen Class file now fixed for PEP8

--- inc/DetermPwgen.py
+++ inc/DetermPwgen.py
@@ -12,17 +12,19 @@
 """
 
 
-import hashlib, base64
+import hashlib
+import base64
+
 
 class DetermPwgen:
 
-	def __init__(self, seed):
-		self._seed = seed
+    def __init__(self, seed):
+        self._seed = seed
 
-	def generate_password(self, hostname, username, rounds):
-		pw = self._seed + hostname + username
+    def generate_password(self, hostname, username, rounds):
+        pw = self._seed + hostname + username
 
-		for i in range(rounds):
-			pw = base64.b64encode((hashlib.sha256(pw).digest()))
+        for i in range(rounds):
+            pw = base64.b64encode((hashlib.sha256(pw).digest()))
 
-		return pw
+        return pw
